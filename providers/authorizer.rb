517c349e032dc12fa87c19cd41c625e1b6d69a78
Andrey Chernih <andrey.chernih@gmail.com>
2013-05-12 21:53:40 +0400

    Remove old code

--- providers/authorizer.rb
+++ providers/authorizer.rb
@@ -1,6 +1,5 @@
 action :authorize do
   template "/home/#{new_resource.username}/.ssh/authorized_keys" do
-    cookbook "ssh"
     source "authorized_keys.erb"
     owner new_resource.username
     group new_resource.username
