2fd66672f4407de7610b34b6fc212794de87df8b
Andrey Chernih <andrey.chernih@gmail.com>
2013-05-12 21:56:06 +0400

    We actually need cookbook name

--- providers/authorizer.rb
+++ providers/authorizer.rb
@@ -1,5 +1,6 @@
 action :authorize do
   template "/home/#{new_resource.username}/.ssh/authorized_keys" do
+    cookbook 'chef-rails-ssh'
     source "authorized_keys.erb"
     owner new_resource.username
     group new_resource.username
